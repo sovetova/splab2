44caa4c88eb8446d6e1fbb9f9119ce8cdb0ede68
sovetova <43229952+sovetova@users.noreply.github.com>
2019-11-07 22:11:31 +0600

    Update mathdo.c

--- MATHDO/mathdo.c
+++ MATHDO/mathdo.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
-int main(int argc, char* argv[]){
-	int pid = fork(); // child1
-	if (pid==0){ // child 1
-		execlp("./add.py",":)",argv[1],argv[2],NULL);
-
-	} else // parent
-	{
-		int pid2 = fork();
-		if (pid2==0){ // child 2
-			execlp("./div.sh",":)",argv[1],argv[2],NULL);
-		} else // parent
-		{
-			wait(0);
-			wait(0);
-			printf("parent: done\n");
-		}		
+int main(int argc, char *argv[]){
+	int i,pid1,pid2,pid3,pid4,count;
+	int status;
+	if(argc!=3){
+		printf("incorrect number of arguments\n");
+		exit(EXIT_FAILURE);
 	}
+	int A,B,S;
+pid1 = fork();
+    if (pid1 == 0){   
+        execlp("python3",":)","add.py",argv[1],argv[2],NULL);
+        exit(EXIT_SUCCESS);
+    }
+
+    pid2 = fork();
+    if (pid2 == 0){   
+        execlp("java",":)","subtraction.java",argv[1],argv[2],NULL);
+        exit(EXIT_SUCCESS);
+    }
+    
+    pid3 = fork();
+    if (pid3 == 0){   
+        execlp("node",":)","multiplication.js",argv[1],argv[2],NULL);
+        exit(EXIT_SUCCESS);
+    }
+    
+    pid4 = fork();
+    if (pid4 == 0){   
+        execlp("bash",":)","division.sh",argv[1],argv[2],NULL);
+        exit(EXIT_SUCCESS);
+    }
+
+    for (i = 0; i < 3; ++i){
+        wait(&status);
+    }
+
+    printf("parent: done. \n");
+    exit(EXIT_SUCCESS);
 }
