67a642c5147a5fe82047c2781362428f7b6573cf
sovetova <43229952+sovetova@users.noreply.github.com>
2019-11-11 23:37:47 +0600

    Update pshell.c

--- PSHELL/pshell.c
+++ PSHELL/pshell.c
@@ -3,35 +3,35 @@
 #include <sys/types.h>
 #include <wait.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <wait.h>
-#include <stdlib.h>
+#include <unistd.h> //fork
+#include <sys/types.h> //fork, _exit
+#include <wait.h> //wait
+#include <stdlib.h> //exit, atoi
 #include <string.h>
 
 int main(int argc, char* argv[]){
 
-	char input[1000];
-	char* arr[100];
+	char s[1000]; //hello#world#of#linux
+	char* t[100]; // 0 1 2 3
 
-	arr[0] = "/bin/bash";
-	arr[1] = "-c";
-	arr[3] = NULL;
+	t[0] = "/bin/bash";
+	t[1] = "-c";
+	t[3] = NULL;
 	do {
 		printf("$ Enter your command: ");
-		fgets(input, 1000, stdin);
+		fgets(s, 1000, stdin);
 		char *pch = strtok(input, "\n");
-		arr[2] = input;
+		t[2] = input;
 
 		if(strcmp(pch, "exit") == 0){
 		break;
 		}
 		if(strcmp(pch, "showjobs") == 0){
-      arr[2] = "ps | awk '$4 != \"pshell\" && $4 != \"bash\" && $4 != \"awk\" && $4 != \"ps\" && $4 != \"CMD\"' | awk '{print NR\".\", \"[\"$1\"]\"}'";
+      t[2] = "ps | awk '$4 != \"pshell\" && $4 != \"bash\" && $4 != \"awk\" && $4 != \"ps\" && $4 != \"CMD\"' | awk '{print NR\".\", \"[\"$1\"]\"}'";
 		}		
 		pid_t p = fork();
 		if (p == 0){
-			execvp(arr[0], arr);
+			execvp(t[0], arr);
 		}
 		waitpid(p, NULL,0);
 	}
